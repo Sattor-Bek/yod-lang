ea579d48b44b4179972d0a713b724c65a6b1b7f4
Satoru Nishizawa <satoru.nh@gmail.com>
2019-12-21 03:03:38 +0900

    api getter

--- app/controllers/subtitles_controller.rb
+++ app/controllers/subtitles_controller.rb
@@ -1,2 +1,10 @@
 class SubtitlesController < ApplicationController
+
+    def call_api(id)
+    url = "https://www.googleapis.com/youtube/v3/videos?id=#{id}&key=#{ENV['GOOGLE_API_KEY']}&part=snippet,contentDetails,statistics,status"
+    info = JSON.parse(RestClient.get(url).body)
+    title = info["items"].first["snippet"]["title"]
+    channel_title = info["items"].first["snippet"]["channelTitle"]
+    { info: info, title: title, id: id, channel_title: channel_title }
+  end
 end
