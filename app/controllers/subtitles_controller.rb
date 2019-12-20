8c8aa355ebdce67ed15a851e34d128585fd415a5
Satoru Nishizawa <satoru.nh@gmail.com>
2019-12-21 03:50:07 +0900

    api needs to get fixed

--- app/controllers/subtitles_controller.rb
+++ app/controllers/subtitles_controller.rb
@@ -1,10 +1,60 @@
 class SubtitlesController < ApplicationController
 
-    def call_api(id)
+  def show
+    authorize @subtitle
+  end
+  def create
+    url = subtitle_params[:video_id]
+    if params[:language] == '日本語(ja)'
+      language = 'ja'
+    elsif params[:language] == '英語(en)'
+      language = 'en'
+    elsif params[:language] == 'ロシア語(ru)'
+      language = 'ru'
+    elsif params[:language] == 'ウズベク語(uz)'
+      language = 'uz'
+    else
+      language = 'en'
+    end
+
+    begin
+      video_id = CGI::parse(URI(url).query)["v"].first
+      video_info = call_api(video_id)
+      @subtitle = subtitle.find_or_create_by(language: language, video_id: video_id, user: current_user, video_title: video_info[:title]) do |subtitle|
+        subtitle.assign_attributes(params_new[:subtitle])
+      end
+    rescue  Subtitle::MissingSubtitlesError
+      @subtitle = Subtitle.new
+      @subtitle.errors.add(:video_id, 'このビデオでは字幕が見つかりません。他のビデオをお試しください。')
+    rescue NoMethodError
+      @subtitle = Subtitle.new(video_id: url)
+      @subtitle.errors.add(:video_id, '無効なURLです。')
+    end
+
+    authorize @subtitle
+
+    if @subtitle.persisted?
+      redirect_to subtitle_path(@subtitle)
+    else
+      render 'pages/home'
+    end
+  end
+
+  def call_api(id)
     url = "https://www.googleapis.com/youtube/v3/videos?id=#{id}&key=#{ENV['GOOGLE_API_KEY']}&part=snippet,contentDetails,statistics,status"
     info = JSON.parse(RestClient.get(url).body)
     title = info["items"].first["snippet"]["title"]
     channel_title = info["items"].first["snippet"]["channelTitle"]
     { info: info, title: title, id: id, channel_title: channel_title }
+  end
+
+  private
+
+  def subtitle_params
+    params.require(:subtitle).permit(:video_id)
+  end
+
+  def set_subtitle
+    @subtitle = Subtitle.find(params[:id])
   end
 end
