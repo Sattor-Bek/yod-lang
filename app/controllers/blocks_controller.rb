791bb4c9e60d3fbc3bfe6bbd137de5d6c1eb59f7
Satoru Nishizawa <satoru.nh@gmail.com>
2020-01-01 19:48:47 +0100

    deleted trash codes

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,11 +1,2 @@
 class BlocksController < ApplicationController
-
-  def index
-    @blocks = Block.where(subtitle_id: params[:subtitle_id])
-
-    respond_to do |format|
-      format.html
-      format.csv { send_data @blocks.as_csv }
-    end
-  end
 end
