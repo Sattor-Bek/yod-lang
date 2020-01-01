48bbc417adbfd6739e0e8ce9c10c984188447fe9
Satoru Nishizawa <satoru.nh@gmail.com>
2020-01-01 18:46:13 +0100

    deleted the block CSV

--- app/models/block.rb
+++ app/models/block.rb
@@ -2,12 +2,5 @@ class Block < ApplicationRecord
   belongs_to :subtitle
   has_many :cards, dependent: :destroy
   validates :sentence, presence: true
-  def to_csv
-    CSV.generate do |csv|
-      csv << column_names
-      all.each do |item|
-        csv << item.attributes.values_at(@subtitle)
-      end
-    end
-  end
+
 end
