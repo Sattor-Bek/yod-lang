0459a180ea29a4fb6f551a142c21a3e0b3be9c63
Satoru Nishizawa <satoru.nh@gmail.com>
2020-01-01 13:55:11 +0100

    added methods

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,2 +1,14 @@
 class BlocksController < ApplicationController
+  before_action :set_block, only: [:show]
+
+  def show
+    respond_to do |format|
+      format.html
+      format.csv { render text: @blocks.to_csv }
+    end
+  end
+
+  def set_block
+    @blocks = Block.where(subtitle_id: params[:subtitle_id])
+  end
 end
--- app/models/block.rb
+++ app/models/block.rb
@@ -2,4 +2,13 @@ class Block < ApplicationRecord
   belongs_to :subtitle
   has_many :cards, dependent: :destroy
   validates :sentence, presence: true
+
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |block|
+        csv << block.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
