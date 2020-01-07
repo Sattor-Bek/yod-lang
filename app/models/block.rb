9fdcdabbf8b24088c0cffb6912943479ba34f724
Satoru Nishizawa <satoru.nh@gmail.com>
2020-01-07 21:10:03 +0100

    now there is a function to get both subtitles

--- app/models/block.rb
+++ app/models/block.rb
@@ -18,37 +18,47 @@ class Block < ApplicationRecord
   end
 
   def self.to_csv(first, second)
-    original = first.sort_by{ |block| block.created_at }.map.with_index{ |item, i| {first_index: i, first_timestamp: item.start_timestamp.to_s, first_subtitle: item.sentence } }
-    to_add = second.sort_by{ |block| block.created_at }.map.with_index{ |item, i|  {second_index: i, second_timestamp: item.start_timestamp.to_s, second_subtitle: item.sentence } }
-    original.append(to_add).flatten!
+    first_block = first.sort_by{ |block| block.created_at }.map.with_index{ |item, i| {first_index: i, first_timestamp: item.start_timestamp.to_s, first_subtitle: item.sentence } }
+    second_block = second.sort_by{ |block| block.created_at }.map.with_index{ |item, i|  {second_index: i, second_timestamp: item.start_timestamp.to_s, second_subtitle: item.sentence } }
 
-    CSV.generate do |csv|
-      column_names = %w(first_timestamp first_subtitle second_timestamp second_subtitle )
-      csv << column_names
-      original.each do |item|
-        if item[:first_timestamp] != nil
+    if first_block.size > second_block.size
+      first_block.each do |first|
+        second_block.each do |second|
+          first.merge!(second) if first[:first_index] == second[:second_index]
+        end
+      end
+      CSV.generate do |csv|
+        column_names = %w(first_timestamp first_subtitle second_timestamp second_subtitle )
+        csv << column_names
+        first_block.each do |item|
           column_values = [
             item[:first_timestamp],
-            item[:first_subtitle].
-            nil,
-            nil
+            item[:first_subtitle],
+            item[:second_timestamp],
+            item[:second_subtitle]
           ]
-        else
-          # column_values.each do |row|
-          #   if row[2] = nil
-          #     row[2] = item[:second_timestamp]
-          #     row[3] = item[:second_subtitle]
-          #   elsif
-          #   end
-          # end
+          csv << column_values
         end
-        csv << column_values
-      raise
-      # if the first block is bigger
-      # put "second" at first with nils and "first" to replace nil (overwrite)
-      # if the second block is bigger
-      # put "first" at first with nils and "second" to replace nil (over write)
-
+      end
+    else
+      second_block.each do |first|
+        first_block.each do |second|
+          first.merge!(second) if first[:second_index] == second[:first_index]
+        end
+      end
+      CSV.generate do |csv|
+        column_names = %w(first_timestamp first_subtitle second_timestamp second_subtitle )
+        csv << column_names
+        second_block.each do |item|
+            column_values = [
+              item[:first_timestamp],
+              item[:first_subtitle],
+              item[:second_timestamp],
+              item[:second_subtitle]
+             ]
+          csv << column_values
+        end
+      end
     end
   end
 end
