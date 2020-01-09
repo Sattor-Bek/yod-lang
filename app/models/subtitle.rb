bd26c5745688a0e7f8c0a01d3afd0a49ed6efa8c
Satoru Nishizawa <satoru.nh@gmail.com>
2020-01-09 21:45:12 +0900

    deleted useless defs

--- app/models/subtitle.rb
+++ app/models/subtitle.rb
@@ -9,20 +9,4 @@ class Subtitle < ApplicationRecord
   def to_param
   return self.url_id
   end
-
-  def self.to_csv(value)
-    CSV.generate do |csv|
-      column_names = %w(start_timestamp, sentence, created_at, updated_at)
-      csv << column_names
-      value.each do |item|
-        column_values = [
-          item.start_timestamp,
-          item.sentence,
-          item.created_at,
-          item.updated_at
-        ]
-        csv << column_values
-      end
-    end
-  end
 end
--- app/models/translation.rb
+++ app/models/translation.rb
@@ -10,19 +10,4 @@ class Translation < ApplicationRecord
   return self.url_id
   end
 
-  def self.to_csv
-    CSV.generate do |csv|
-      column_names = %w(start_timestamp, sentence, created_at, updated_at)
-      csv << column_names
-      value.each do |item|
-        column_values = [
-          item.start_timestamp,
-          item.sentence,
-          item.created_at,
-          item.updated_at
-        ]
-        csv << column_values
-      end
-    end
-  end
 end
