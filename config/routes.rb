7508ddbc6f3462eb05f16650a991106deaceee03
Satoru Nishizawa <satoru.nh@gmail.com>
2020-02-10 00:57:59 +0900

    routes for forum

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'forums/new'
+  get 'forums/create'
+  get 'forums/edit'
+  get 'forums/update'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   # post 'pages/guest' => 'pages#guest'
   post '/pages/guest_sign_in', to: 'pages#new_guest'
+  resources :forums, only: [:index, :create, :show, :edit, :update] do
+    resources :pots
+  end
 
   resources :subtitles, only: [:index, :create, :show, :eidt, :update], param: :url_id do
     resources :translations, only: [:index, :create, :show], param: :url_id
